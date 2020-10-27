ecc86ab0e1cbc7e6fbe123239a0b11899063ee4a
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-10-27 17:23:43 +0100

    added getSortedRecords

--- EXIFReader/EXIFStats.swift
+++ EXIFReader/EXIFStats.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
+struct EXIFRecord {
+    var key:String
+    var count:Int
+    var percentage:String
+}
+
 struct EXIFStats {
+    var numberOfRecords:Int {
+        records.keys.count
+    }
+    var sortedRecords:[EXIFRecord] {
+        getSortedRecords()
+    }
     var records:[String:Int] = [:]
     
     mutating func updateWithValue(_ value:String) {
@@ -17,5 +29,21 @@ struct EXIFStats {
         else {
             records[value] = 1
         }
+    }
+    
+    private func getSortedRecords() -> [EXIFRecord] {
+        var sortedArray:[EXIFRecord] = []
+        let totalCount = records.values.reduce(0) { (x, y) in
+            x + y
+        }
+        for (key, value) in records {
+            let percentage = Float(value) / Float(totalCount) * 100
+            let percentageStr = String(format: "%2.f %%", percentage)
+            sortedArray.append(EXIFRecord(key: key, count: value, percentage: percentageStr))
+        }
+        sortedArray.sort { (a, b) -> Bool in
+            a.count > b.count
+        }
+        return sortedArray
     }
 }
