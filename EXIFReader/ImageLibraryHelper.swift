98156d5faa13375460ec12bfae2d4de58030b3cf
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-10-27 15:32:59 +0100

    added callback

--- EXIFReader/ImageLibraryHelper.swift
+++ EXIFReader/ImageLibraryHelper.swift
@@ -12,10 +12,13 @@ import UIKit
 typealias EXIFData = [String:Any]
 
 class ImageLibraryHelper {
-    func getExifDataFromLibrary() {
+    func getExifDataFromLibrary(completion:@escaping (Bool, EXIFStats) -> Void) {
+        self.completionHandler = completion
         getPhotos()
     }
     
+    private var completionHandler:((Bool, EXIFStats) -> Void)?
+    private var counter = 0
     private var exifStats = EXIFStats()
     private var exifKey = "LensModel"
     
@@ -29,15 +32,19 @@ class ImageLibraryHelper {
 
         let results: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
         if results.count > 0 {
+            counter = results.count
             for i in 0..<results.count {
                 let asset = results.object(at: i)
                 manager.requestImageDataAndOrientation(for: asset, options: requestOptions) { (data, fileName, orientation, info) in
                     if let data = data,
                        let cImage = CIImage(data: data) {
                         if let exif = cImage.properties["{Exif}"] as? [String:Any] {
-                            print("EXIF Data: \(exif)")
                             if let value = exif[self.exifKey] as? String {
                                 self.exifStats.updateWithValue(value)
+                            }
+                            self.counter -= 1
+                            if self.counter == 0 {
+                                self.completionHandler?(true, self.exifStats)
                             }
                         }
                     }
